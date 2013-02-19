9e384cdee310ffc9d83c47095f7467fb5fbaa9ce
zhangxiaowei <zhangxiaowei.hust@gmail.com>
2013-02-19 09:25:18 +0800

    github does not support chinese.........
    change all the chinese to english

--- changeIP.bat
+++ changeIP.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL
 
-set eth="本地连接"
+set eth="xxx"   rem your Ethernet card name
 
 
 
@@ -16,7 +16,7 @@ call :main
 goto :eof
 
 :main
-set /p input="继续请按任意键,退出请按q:"
+set /p input="press any key to contine,'q' to exit:"
 if "%input%"=="q" (set a=3) else (call :ChangeIP)
 if %a% lss 2 (call :main) 
 goto :eof
@@ -33,13 +33,13 @@ if %last% EQU 255 set last=1
 
 set nextIP=%first%%last%
 echo.
-echo 正在将IP设置更改到：[%nextIP%] [%mask%] [%gateway%]
+echo changing current ip to：[%nextIP%] [%mask%] [%gateway%]
 
 netsh interface ip set address name=%eth% source=static addr=%nextIP% mask=%mask% gateway=%gateway% gwmetric=auto 
 
 echo ---------------------------------
 
-echo   更改完毕
+echo  change finished 
 
 echo ---------------------------------
 echo.           
